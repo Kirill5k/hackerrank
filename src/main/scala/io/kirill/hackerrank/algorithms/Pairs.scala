284d69267d75c9abae9dcd9fd9270a035f400086
kirill5k <immotional@aol.com>
2020-07-12 11:02:13 +0100

    cleanup

--- src/main/scala/io/kirill/hackerrank/algorithms/Pairs.scala
+++ src/main/scala/io/kirill/hackerrank/algorithms/Pairs.scala
@@ -4,8 +4,6 @@ object Pairs {
 
   def pairs(k: Int, arr: Array[Int]): Int = {
     val nums = arr.toSet
-    arr.foldLeft(0) {
-      case (res, el) => if (nums.contains(el+k)) res + 1 else res
-    }
+    arr.foldLeft(0) { (res, el) => if (nums.contains(el+k)) res + 1 else res }
   }
 }
--- src/test/scala/io/kirill/hackerrank/algorithms/PairsSpec.scala
+++ src/test/scala/io/kirill/hackerrank/algorithms/PairsSpec.scala
@@ -8,7 +8,6 @@ class PairsSpec extends AnyWordSpec with Matchers {
   "Pairs::pairs" should {
 
     "return amount of pairs with a given remainder" in {
-
       Pairs.pairs(2, Array(1,5,3,4,2)) must be (3)
     }
   }
