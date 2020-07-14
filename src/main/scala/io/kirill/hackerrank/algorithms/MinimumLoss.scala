684b121f2b998a7132cca391303e5e010c029160
kirill5k <immotional@aol.com>
2020-07-14 09:05:18 +0100

    refactored minimum loss

--- src/main/scala/io/kirill/hackerrank/algorithms/MinimumLoss.scala
+++ src/main/scala/io/kirill/hackerrank/algorithms/MinimumLoss.scala
@@ -3,15 +3,16 @@ package io.kirill.hackerrank.algorithms
 object MinimumLoss {
 
   def minimumLoss(price: Array[Long]): Int = {
-    val sorted = price.sorted.reverse
-    var loss = Long.MaxValue
-    for (i <- 0 until price.length - 1) {
-      val (p1, p2) = (sorted(i), sorted(i+1))
-      if (p1 - p2 < loss && price.indexOf(p1) < price.indexOf(p2)) {
-        loss = p1 - p2
+    price
+      .sorted
+      .reverse
+      .sliding(2)
+      .filter {
+        case Array(p1, p2) => price.indexOf(p1) < price.indexOf(p2)
       }
-    }
-
-    loss.toInt
+      .map {
+        case Array(p1, p2) => (p1 - p2).toInt
+      }
+      .min
   }
 }
