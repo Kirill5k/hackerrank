fc7139bc1ab33ae59e09a933abfbc26b4a1fa7df
kirill5k <immotional@aol.com>
2020-07-16 18:46:06 +0100

    updated absolute permutation solution

--- src/main/scala/io/kirill/hackerrank/algorithms/AbsolutePermutation.scala
+++ src/main/scala/io/kirill/hackerrank/algorithms/AbsolutePermutation.scala
@@ -4,15 +4,15 @@ object AbsolutePermutation {
 
   def absolutePermutation(n: Int, k: Int): Array[Int] = {
     if (k == 0) (1 to n).toArray
-    else if ((n/k) % 2 == 0) {
+    else if ((n.toDouble/k) % 2 != 0) Array(-1)
+    else {
       var j = k + 1
-      var result = List[Int]()
+      val result = scala.collection.mutable.ListBuffer[Int]()
       while (result.size < n) {
-        (0 until k).foreach(_ => result = result :+ result.size + j)
+        (0 until k).foreach(_ => result.append(result.size + j))
         j = if (j == 1 + k) 1 - k else 1 + k
       }
       result.toArray
     }
-    else Array(-1)
   }
 }
