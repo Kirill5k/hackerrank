3d203058ab9b025240a97a18c789602fedf350e1
kirill5k <immotional@aol.com>
2020-07-17 15:15:23 +0100

    updated fraud activities solution

--- src/main/scala/io/kirill/hackerrank/algorithms/FraudulentActivityNotifications.scala
+++ src/main/scala/io/kirill/hackerrank/algorithms/FraudulentActivityNotifications.scala
@@ -2,24 +2,66 @@ package io.kirill.hackerrank.algorithms
 
 object FraudulentActivityNotifications {
 
-  def activityNotifications(expenditure: Array[Int], d: Int): Int = {
-    val e = d % 2 == 0
-    val mid = d/2
-    var notifications = 0
-    for (i <- 0 until expenditure.length - 1 - d) {
-      val past = expenditure.slice(i, i+d).sorted
-      val today = expenditure(i+d)
-      val med = median(past, mid, e)
-      if (med * 2 <= today) {
-        notifications += 1
-      }
+  /*
+
+  function activityNotifications (expenditure, d) {
+
+    // Number of notifications
+    let n = 0
+
+    // Set midpoints for median calculation
+    let [ i1, i2 ] = [ Math.floor((d-1)/2), Math.ceil((d-1)/2) ]
+    let m1, m2, m
+
+    // Initialize count sorted subarray
+    let cs = new Array(201).fill(0)
+    for (let i = d-1; i >= 0; i--) cs[expenditure[i]]++
+
+    // Iterate through expenditures
+    for (let i = d, l = expenditure.length; i < l; i++) {
+
+        // Find median
+        for (let j = 0, k = 0; k <= i1; k += cs[j], j++) m1 = j
+        for (let j = 0, k = 0; k <= i2; k += cs[j], j++) m2 = j
+        let m = (m1 + m2) / 2
+
+        // Check if notification is given
+        if (expenditure[i] >= m * 2) n++
+
+        // Replace subarray elements
+        cs[expenditure[i-d]]--
+        cs[expenditure[i]]++
     }
 
+    return n
+}
+
+   */
+
+
+  def activityNotifications(expenditure: Array[Int], d: Int): Int = {
+    var notifications = 0
+    val (i1, i2) = (math.floor((d-1)/2.0).toInt, math.ceil((d-1)/2.0).toInt)
+    val counts = Array.fill(201)(0)
+    (0 until d).foreach(i => counts(expenditure(i)) = counts(expenditure(i)) + 1)
+    for (i <- d until expenditure.length) {
+      val m1 = getMedianPoint(counts, i1)
+      val m2 = getMedianPoint(counts, i2)
+      val m = (m1+m2)/2.0
+      if (expenditure(i) >= m * 2) {
+        notifications += 1
+      }
+      counts(expenditure(i-d)) = counts(expenditure(i-d)) - 1
+      counts(expenditure(i)) = counts(expenditure(i-d)) + 1
+    }
     notifications
   }
 
-  def median(expenses: Array[Int], mid: Int, e: Boolean): Double = {
-    if (e) (expenses(mid) + expenses(mid+1)) / 2.0
-    else expenses(mid)
+  @scala.annotation.tailrec
+  def getMedianPoint(counts: Array[Int], i: Int, k: Int = 0, j: Int = 0): Int = {
+    if (k > i) j
+    else {
+      getMedianPoint(counts, i, k + counts(j), j+1)
+    }
   }
 }
--- src/test/scala/io/kirill/hackerrank/algorithms/FraudulentActivityNotificationsSpec.scala
+++ src/test/scala/io/kirill/hackerrank/algorithms/FraudulentActivityNotificationsSpec.scala
@@ -7,9 +7,9 @@ class FraudulentActivityNotificationsSpec extends AnyWordSpec with Matchers {
 
   "FraudulentActivityNotifications::activityNotifications" should {
     "return amount of notification that will be sent" in {
-//      FraudulentActivityNotifications.activityNotifications(Array(10,20,30,40,50), 3) must be (1)
+      FraudulentActivityNotifications.activityNotifications(Array(10,20,30,40,50), 3) must be (1)
       FraudulentActivityNotifications.activityNotifications(Array(2,3,4,2,3,6,8,4,5), 5) must be (2)
-//      FraudulentActivityNotifications.activityNotifications(Array(1,2,3,4,4), 4) must be (0)
+      FraudulentActivityNotifications.activityNotifications(Array(1,2,3,4,4), 4) must be (0)
     }
   }
 }
