b936e7aa7f7bca6c0c455bbac8355c025316a455
kirill5k <immotional@aol.com>
2020-07-20 11:02:56 +0100

    complete solution

--- src/main/scala/io/kirill/hackerrank/algorithms/BreadthFirstSearchShortestReach.scala
+++ src/main/scala/io/kirill/hackerrank/algorithms/BreadthFirstSearchShortestReach.scala
@@ -2,51 +2,38 @@ package io.kirill.hackerrank.algorithms
 
 object BreadthFirstSearchShortestReach {
 
+  import scala.collection.immutable.{List, Set}
+
   def bfs(nNodes: Int, nEdges: Int, edges: Array[Array[Int]], s: Int): Array[Int] = {
     val visited   = Array.fill(nNodes + 1)(false)
-    val distances = Array.fill(nNodes + 1)(0)
-    distances(s) = s
-    val graph = Array.fill(nNodes + 1)(scala.collection.immutable.Set[Int]())
+    visited(s) = true
+    val distances = Array.fill(nNodes + 1)(-1)
+    distances(s) = 0
+    val graph = Array.fill(nNodes + 1)(Set[Int]())
     edges.foreach {
       case Array(a, b) =>
         graph(a) = graph(a) + b
         graph(b) = graph(b) + a
     }
 
-    val travelledDist = search(scala.collection.immutable.List(s), visited, distances, graph)
+    val travelledDist = search(List(s), visited, distances, graph)
 
-    (1 to nNodes)
-      .map(n => (n, getDistance(n, travelledDist, 0)))
-      .filter(_._1 != s)
-      .map(_._2)
-      .map(d => if (d == 0) -1 else d * 6)
-      .toArray
+    travelledDist
+      .drop(1)
+      .filter(_ != 0)
+      .map(d => if (d == - 1) d else d * 6)
   }
 
   @scala.annotation.tailrec
-  def search(
-      queue: scala.collection.immutable.List[Int],
-      visited: Array[Boolean],
-      distances: Array[Int],
-      graph: Array[scala.collection.immutable.Set[Int]]
-  ): Array[Int] =
-    if (queue.isEmpty) distances
+  def search(queue: List[Int], vis: Array[Boolean], dist: Array[Int], graph: Array[Set[Int]]): Array[Int] =
+    if (queue.isEmpty) dist
     else {
       val n = queue.head
-      if (!visited(n)) {
-        visited(n) = true
-        val an = graph(n).filter(x => !visited(x))
-        an.foreach(x => distances(x) = n)
-        search(an ++: queue.tail, visited, distances, graph)
-      } else {
-        search(queue.tail, visited, distances, graph)
-      }
+      val adjacentNodes = graph(n).filter(x => !vis(x) && dist(x) == -1)
+      adjacentNodes.foreach { an =>
+          dist(an) = dist(n) + 1
+          vis(an) = true
+        }
+      search(queue.tail :++ adjacentNodes, vis, dist, graph)
     }
-
-  @scala.annotation.tailrec
-  def getDistance(n: Int, travelledDist: Array[Int], journey: Int): Int = {
-    val prev = travelledDist(n)
-    if (prev == n || prev == 0) journey
-    else getDistance(prev, travelledDist, journey + 1)
-  }
 }
