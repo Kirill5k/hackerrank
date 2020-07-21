79c7af1282dd32f66bea03e1021a30b6e0d09a8a
kirill5k <immotional@aol.com>
2020-07-21 10:03:25 +0100

    cleanup

--- src/main/scala/io/kirill/hackerrank/algorithms/AlmostSorted.scala
+++ src/main/scala/io/kirill/hackerrank/algorithms/AlmostSorted.scala
@@ -2,6 +2,8 @@ package io.kirill.hackerrank.algorithms
 
 object AlmostSorted {
 
+  import scala.collection.immutable.List
+
   def canBeSorted(arr: Array[Int]): Array[String] = {
     val peaks = (1 until arr.length - 1).map { i =>
       val (p, c, n) = (arr(i-1), arr(i), arr(i+1))
@@ -9,19 +11,19 @@ object AlmostSorted {
       (i+1, inv)
     }.toList
 
-    (peaks.count(_._2 == 1), peaks.count(_._2 == -1)) match {
+    val groupedPeaks = peaks.groupBy(_._2)
+
+    (groupedPeaks.get(1).map(_.size).getOrElse(0), groupedPeaks.get(-1).map(_.size).getOrElse(0)) match {
       case (0, 0) =>
         if (arr.length == 2 && arr(0) > arr(1)) Array("yes", "swap 1 2")
         else Array("yes")
       case (1, 1) =>
-        val p1 = peaks.find(_._2 == 1).map(_._1).get
-        val p2 = peaks.findLast(_._2 == -1).map(_._1).get
+        val (p1, p2) = (groupedPeaks(1).head._1, groupedPeaks(-1).head._1)
         if (p1 + 1 != p2) Array("yes", s"reverse $p1 $p2")
         else if (canBeSwapped(p1-1, p2-1, arr)) Array("yes", s"swap $p1 $p2")
         else Array("no")
       case (2, 2) =>
-        val p1 = peaks.find(_._2 == 1).map(_._1).get
-        val p2 = peaks.findLast(_._2 == -1).map(_._1).get
+        val (p1, p2) = (groupedPeaks(1).head._1, groupedPeaks(-1).last._1)
         if (canBeSwapped(p1-1, p2-1, arr)) Array("yes", s"swap $p1 $p2") else Array("no")
       case (_, _) =>
         Array("no")
