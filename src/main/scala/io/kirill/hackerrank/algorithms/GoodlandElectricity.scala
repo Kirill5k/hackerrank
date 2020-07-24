55040dd0e64f2bf64901a36dea4b0f6b9032777a
kirill5k <immotional@aol.com>
2020-07-24 09:33:57 +0100

    goodland electricity sol 3

--- src/main/scala/io/kirill/hackerrank/algorithms/GoodlandElectricity.scala
+++ src/main/scala/io/kirill/hackerrank/algorithms/GoodlandElectricity.scala
@@ -6,15 +6,13 @@ object GoodlandElectricity {
     @scala.annotation.tailrec
     def go(pos: Int, coverage: Int, towers: Int): Int = {
       if (coverage >= arr.length) towers
+      else if (pos < coverage - k + 1 || pos < 0) -1
       else {
         if (arr(pos) == 1) {
           val newCoverage = pos + k
           val newPos = newCoverage + k - 1
           if (newPos >= arr.length) go(arr.length-1, newCoverage, towers+1) else go(newPos, newCoverage, towers+1)
-        } else {
-          if (pos <= coverage - k + 1 || pos <= 0) -1
-          else go(pos-1, coverage, towers)
-        }
+        } else go(pos-1, coverage, towers)
       }
     }
 
